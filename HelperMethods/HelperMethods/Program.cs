79976544b3620a1e8b079de7ddf537155e981749
lobersky <l.obersky@gmail.com>
2018-03-26 16:36:41 +1000

    Overloaded Methods
    Tutorial 11 - Creating and calling ocerloaded helper methods.

---
+++ HelperMethods/HelperMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myValue = superSecretFormula("sunshine");
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+        }
+        // Tutorial 11 - Helper Methods
+        // Here we have created an overloaded method with same name but different input parameters. 
+
+        private static string superSecretFormula()
+        {
+            // some cool stuff here
+            return "Hello World!";
+        }
+        private static string superSecretFormula(string name)
+        {
+            return String.Format("Hello, {0}!", name);
+        }
+    }
+
+}
