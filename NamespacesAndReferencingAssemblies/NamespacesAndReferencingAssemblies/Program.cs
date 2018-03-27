c9d2ec69e6083c8365b3b16c25724a4c2b4b79ad
lobersky <l.obersky@gmail.com>
2018-03-27 16:03:33 +1000

    Namesspaces and Referencing Assemblies
    Concept of namespaces as an organisation tool for libraries; reference assemblies (dll) files; how to add external reference assemblies to Visual Studio.

---
+++ NamespacesAndReferencingAssemblies/NamespacesAndReferencingAssemblies/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabor;
+
+namespace NamespacesAndReferencingAssemblies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // System.IO.StreamReader myStreamReader = new System.IO.StreamReader(); use full when ambigious classes case!
+            // StreamReader myStreamReader = new StreamReader();
+
+            // Bob created a library that can pull html info from website URL defined
+            Bob bob = new Bob();
+            string html = bob.Lookup("http://www.learnvisualstudio.net");
+            Console.WriteLine(html);
+            Console.ReadLine();
+        }
+    }
+}
